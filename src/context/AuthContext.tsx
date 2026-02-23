d6233e31e6d68bc88712f3acbf6ab76eb4f08ebc
Idan <elefantidan@gmail.com>
2026-02-23 21:49:53 +0200

    fix: imports syntax syntax in AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import type { ReactNode } from 'react';
-signInWithEmailAndPassword,
+import {
+    signInWithEmailAndPassword,
     createUserWithEmailAndPassword,
     signOut,
     onAuthStateChanged,
